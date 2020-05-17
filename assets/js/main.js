8bd3a59092f54981a1b8036a9f18e2932b3b0f77
Denis Tatar <denis.tatar8@gmail.com>
2020-05-17 19:39:09 -0400

    Got rid of some pieces from layout and added my project

--- assets/js/main.js
+++ assets/js/main.js
@@ -237,6 +237,458 @@
       }
 
 
+    // NEW STUFF FOR MY PROJECTS, BOTH DESIGN AND CODING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Adding a Pop-Up (Project 1) Porsche Car Connect
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+
+
+
+
+    // Adding a Pop-Up (Project 2) Privaseeable
+    // Get the modal
+    var modal2 = document.getElementById("myModal2");
+    // Get the button that opens the modal
+    var btn2 = document.getElementById("myBtn2");
+    // Get the <span> element that closes the modal
+    var span2 = document.getElementsByClassName("close2")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn2.onclick = function () {
+        modal2.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span2.onclick = function () {
+        modal2.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal2) {
+            modal2.style.display = "none";
+        }
+    }
+
+    // Adding a Pop-Up (Project 3) Mood Playlist
+    // Get the modal
+    var modal3 = document.getElementById("myModal3");
+    // Get the button that opens the modal
+    var btn3 = document.getElementById("myBtn3");
+    // Get the <span> element that closes the modal
+    var span3 = document.getElementsByClassName("close3")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn3.onclick = function () {
+        modal3.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span3.onclick = function () {
+        modal3.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal3) {
+            modal3.style.display = "none";
+        }
+    }
+
+
+    // Adding a Pop-Up (Project 4) Personal Portfolio
+    // Get the modal
+    var modal4 = document.getElementById("myModal4");
+    // Get the button that opens the modal
+    var btn4 = document.getElementById("myBtn4");
+    // Get the <span> element that closes the modal
+    var span4 = document.getElementsByClassName("close4")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn4.onclick = function () {
+        modal4.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span4.onclick = function () {
+        modal4.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal4) {
+            modal4.style.display = "none";
+        }
+    }
+
+
+    // Adding a Pop-Up (Project 5) Math It
+    // Get the modal
+    var modal5 = document.getElementById("myModal5");
+    // Get the button that opens the modal
+    var btn5 = document.getElementById("myBtn5");
+    // Get the <span> element that closes the modal
+    var span5 = document.getElementsByClassName("close5")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn5.onclick = function () {
+        modal5.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span5.onclick = function () {
+        modal5.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal5) {
+            modal5.style.display = "none";
+        }
+    }
+
+    // Adding a Pop-Up (Project 6) ML Model Training
+    // Get the modal
+    var modal6 = document.getElementById("myModal6");
+    // Get the button that opens the modal
+    var btn6 = document.getElementById("myBtn6");
+    // Get the <span> element that closes the modal
+    var span6 = document.getElementsByClassName("close6")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn6.onclick = function () {
+        modal6.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span6.onclick = function () {
+        modal6.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal6) {
+            modal6.style.display = "none";
+        }
+    }
+
+    // Adding a Pop-Up (Project 7) RaceIt
+    // Get the modal
+    var modal7 = document.getElementById("myModal7");
+    // Get the button that opens the modal
+    var btn7 = document.getElementById("myBtn7");
+    // Get the <span> element that closes the modal
+    var span7 = document.getElementsByClassName("close7")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn7.onclick = function () {
+        modal7.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span7.onclick = function () {
+        modal7.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal7) {
+            modal7.style.display = "none";
+        }
+    }
+
+    // Adding a Pop-Up (Project 8) Minion Run
+    // Get the modal
+    var modal8 = document.getElementById("myModal8");
+    // Get the button that opens the modal
+    var btn8 = document.getElementById("myBtn8");
+    // Get the <span> element that closes the modal
+    var span8 = document.getElementsByClassName("close8")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn8.onclick = function () {
+        modal8.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span8.onclick = function () {
+        modal8.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal8) {
+            modal8.style.display = "none";
+        }
+    }
+
+
+
+    // DESIGN PROJECTS //
+    // Adding a Pop-Up (Project 1) Dream Big Piece
+    // Get the modal
+    var modald1 = document.getElementById("myModald1");
+    // Get the button that opens the modal
+    var btnd1 = document.getElementById("myBtnd1");
+    // Get the <span> element that closes the modal
+    var spand1 = document.getElementsByClassName("closed1")[0];
+
+    // When the user clicks on the button, open the modal 
+    btnd1.onclick = function () {
+        modald1.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    spand1.onclick = function () {
+        modald1.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modald1) {
+            modald1.style.display = "none";
+        }
+    }
+
+
+    // Adding a Pop-Up (Project 2) Dior X Kaws Piece
+    // Get the modal
+    var modald2 = document.getElementById("myModald2");
+    // Get the button that opens the modal
+    var btnd2 = document.getElementById("myBtnd2");
+    // Get the <span> element that closes the modal
+    var spand2 = document.getElementsByClassName("closed2")[0];
+
+    // When the user clicks on the button, open the modal 
+    btnd2.onclick = function () {
+        modald2.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    spand2.onclick = function () {
+        modald2.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modald2) {
+            modald2.style.display = "none";
+        }
+    }
+
+
+    // Adding a Pop-Up (Project 3) Gucci Piece
+    // Get the modal
+    var modald3 = document.getElementById("myModald3");
+    // Get the button that opens the modal
+    var btnd3 = document.getElementById("myBtnd3");
+    // Get the <span> element that closes the modal
+    var spand3 = document.getElementsByClassName("closed3")[0];
+
+    // When the user clicks on the button, open the modal 
+    btnd3.onclick = function () {
+        modald3.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    spand3.onclick = function () {
+        modald3.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modald3) {
+            modald3.style.display = "none";
+        }
+    }
+
+
+    // Adding a Pop-Up (Project 4) Avu Clothing
+    // Get the modal
+    var modald4 = document.getElementById("myModald4");
+    // Get the button that opens the modal
+    var btnd4 = document.getElementById("myBtnd4");
+    // Get the <span> element that closes the modal
+    var spand4 = document.getElementsByClassName("closed4")[0];
+
+    // When the user clicks on the button, open the modal 
+    btnd4.onclick = function () {
+        modald4.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    spand4.onclick = function () {
+        modald4.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modald4) {
+            modald4.style.display = "none";
+        }
+    }
+
+
+    // Adding a Pop-Up (Project 5) Sunset Illustration
+    // Get the modal
+    var modald5 = document.getElementById("myModald5");
+    // Get the button that opens the modal
+    var btnd5 = document.getElementById("myBtnd5");
+    // Get the <span> element that closes the modal
+    var spand5 = document.getElementsByClassName("closed5")[0];
+
+    // When the user clicks on the button, open the modal 
+    btnd5.onclick = function () {
+        modald5.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    spand5.onclick = function () {
+        modald5.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modald5) {
+            modald5.style.display = "none";
+        }
+    }
+
+
+    // Adding a Pop-Up (Project 6) Strange Fruits Cover Art #1
+    // Get the modal
+    var modald6 = document.getElementById("myModald6");
+    // Get the button that opens the modal
+    var btnd6 = document.getElementById("myBtnd6");
+    // Get the <span> element that closes the modal
+    var spand6 = document.getElementsByClassName("closed6")[0];
+
+    // When the user clicks on the button, open the modal 
+    btnd6.onclick = function () {
+        modald6.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    spand6.onclick = function () {
+        modald6.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modald6) {
+            modald6.style.display = "none";
+        }
+    }
+
+    // Adding a Pop-Up (Project 7) Strange Fruits Cover Art #2
+    // Get the modal
+    var modald7 = document.getElementById("myModald7");
+    // Get the button that opens the modal
+    var btnd7 = document.getElementById("myBtnd7");
+    // Get the <span> element that closes the modal
+    var spand7 = document.getElementsByClassName("closed7")[0];
+
+    // When the user clicks on the button, open the modal 
+    btnd7.onclick = function () {
+        modald7.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    spand7.onclick = function () {
+        modald7.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modald7) {
+            modald7.style.display = "none";
+        }
+    }
+
+    // Adding a Pop-Up (Project 9) Chief Wolf Clothing
+    // Get the modal
+    var modald9 = document.getElementById("myModald9");
+    // Get the button that opens the modal
+    var btnd9 = document.getElementById("myBtnd9");
+    // Get the <span> element that closes the modal
+    var span9 = document.getElementsByClassName("closed9")[0];
+
+    // When the user clicks on the button, open the modal 
+    btnd9.onclick = function () {
+        modald9.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    spand9.onclick = function () {
+        modald9.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modald9) {
+            modald9.style.display = "none";
+        }
+    }
+
+
+    let $btns = $('.project-area .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+
+// Sticky Navigation Menu
+let nav_offset_top = $('.header_area').height() + 50;
+
+function navbarFixed(){
+    if($('.header_area').length){
+        $(window).scroll(function(){
+            let scroll = $(window).scrollTop();
+            if (scroll >= nav_offset_top){
+                $('.header_area .main-menu').addClass('navbar_fixed');
+            } else {
+                $('.header_area .main-menu').removeClass('navbar_fixed');
+            }
+        })
+    }
+}
+
+ navbarFixed();
+ // END TO ALL OF MY BUTTON STUFF FOR CODING AND DESIGN PROJECTS!!!!!!!!!!!!!!!!!!
+
 
 
 })(jQuery);
